33210d478b3e8e33ae0a03b9bd77d105a5d7b3c8
Vincent Desmouceaux <114471831+VincentDesmouceaux@users.noreply.github.com>
2023-05-06 11:25:06 +0200

    initialisation2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import axios from "axios";
+// J'importe Axios qui va permettre de faire des requêtes aux API fournies dans l'énnoncé
+
 import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [popular, setPopular] = useState("");
+  const [fromCity, setFromCity] = useState("");
+
   return (
     <div>
       <h1>Hello World</h1>

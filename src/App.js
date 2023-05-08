866d7f3cd211a0f97d1ae7afbfb691c629305fbb
Vincent Desmouceaux <114471831+VincentDesmouceaux@users.noreply.github.com>
2023-05-08 17:39:43 +0200

    mainfunctiondone

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 // J'importe Axios qui va permettre de faire des requêtes aux API fournies dans l'énnoncé
 
 function App() {
   const [data, setData] = useState([]);
   const [data2, setData2] = useState([]);
+  const [data3, setData3] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [popular, setPopular] = useState("");
   const [show, setShow] = useState(false);
-  const [showButton, setShowButton] = useState(false);
 
   const [fromCity, setFromCity] = useState("");
 
@@ -30,26 +30,7 @@ function App() {
     if (search) {
       firstSearch();
     }
-
-    if (data.length > 0) {
-      const input = document.querySelector(".search-input");
-      input && input.focus();
-    }
-
-    // const thirdSearch = async () => {
-    //   try {
-    //     const response = await axios.get(
-    //       `https://api.comparatrip.eu/cities/popular/from/${fromCity}/5`
-    //     );
-    //     setData(response.data);
-    //     setIsLoading(false);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // };
-
-    // thirdSearch();
-  }, [search, fromCity, data]);
+  }, [search, data]);
 
   useEffect(() => {
     const secondSearch = async () => {
@@ -67,10 +48,26 @@ function App() {
     secondSearch();
   }, [popular]);
 
+  useEffect(() => {
+    const thirdSearch = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.comparatrip.eu/cities/popular/from/${fromCity}/5`
+        );
+        setData3(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (fromCity) {
+      thirdSearch();
+    }
+  }, [fromCity]);
+
   const handleSelectAutoComplete = (autoComplete) => {
     setData([]);
     setSearch(autoComplete.local_name);
-    setShowButton(true);
   };
 
   return isLoading ? (
@@ -85,7 +82,6 @@ function App() {
         onChange={(event) => {
           setSearch(event.target.value);
         }}
-        ref={(input) => input && input.focus()}
       />
       <ul>
         {data.map((autoComplete) => (
@@ -97,15 +93,16 @@ function App() {
           </li>
         ))}
       </ul>
-      {!showButton && (
-        <div className="dropdown">
-          <button className="mostPop" onClick={() => setShow(!show)}>
-            Top 5 cities
-          </button>
-          {show && (
-            <div className="display">
+
+      <div className="dropdown">
+        <button className="mostPop" onClick={() => setShow(!show)}>
+          Top 5 cities
+        </button>
+        {show && (
+          <div className="display">
+            <ul>
               {data2.map((top) => (
-                <button
+                <li
                   key={top.id}
                   onClick={() => {
                     setPopular(top.local_name);
@@ -113,18 +110,40 @@ function App() {
                   }}
                 >
                   {top.local_name}
-                </button>
+                </li>
               ))}
-            </div>
-          )}
-        </div>
-      )}
+            </ul>
+          </div>
+        )}
+      </div>
+
       {!show && popular !== "" && (
         <button className="mostPop" onClick={() => setShow(true)}>
           {popular}
         </button>
       )}
-      <input type="text" />
+      <input
+        value={fromCity}
+        type="text"
+        className="search-input"
+        placeholder="Top 5 popular cities from..."
+        onChange={(event) => {
+          setFromCity(event.target.value);
+        }}
+      />
+      <ul>
+        {data3.map((from) => (
+          <li
+            key={from.id}
+            onClick={() => {
+              setData3([]);
+              setFromCity(from.unique_name);
+            }}
+          >
+            {from.unique_name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
